a812ea5bf724cceeb9990b06f264ca834f56f42e
frkncs <hesapcocnd@gmail.com>
2025-06-15 03:35:52 +0300

    Completed all system

--- armut_job_listing_cleaner.py
+++ armut_job_listing_cleaner.py
@@ -13,58 +13,63 @@ options.debugger_address = "localhost:9222"
 
 driver = webdriver.Chrome(options=options)
 
-print("Opening opportunities tab...")
+print("\"Fırsatlarım\" sayfası açılıyor...")
 
-driver.get("https://pro.armut.com/opportunities")
+while True:
+    driver.get("https://pro.armut.com/opportunities")
 
-wait = WebDriverWait(driver, 10)
+    wait = WebDriverWait(driver, 10)
 
-wait.until(
-    EC.presence_of_all_elements_located((By.XPATH, '//a[contains(@href, "/opportunities/")]'))
-)
-
-print("Opportunities tab loaded.")
-
-input("Press enter if you selected your filters...")
-
-print("Getting all listings available now...")
-
-anchors = driver.find_elements(By.XPATH, '//a[contains(@href, "/opportunities/")]')
-links = [a.get_attribute('href') for a in anchors]
-
-for link in links:
-    print(f"Waiting for {link[::-1][:8][::-1]} opening...")
-    
-    driver.get(f"{link}")
-    
-    reject_button = wait.until(
-        EC.element_to_be_clickable((By.XPATH, '//button[.//span[text()="Reddet"]]'))
-    )
-    
-    print("Page Opened!")
-
-    reject_button.click()
-    
-    print("Clicked to reject button...")
-    
-    radio_button = wait.until(
-        EC.element_to_be_clickable((By.XPATH, '//input[@type="radio" and @value="13"]'))
-    )
-    
-    print("Selected \"Hizmet (meslek) uygun değil\" option...")
-
-    radio_button.click()
-    
-    submit_button = wait.until(
-        EC.element_to_be_clickable((By.XPATH, '//button[.//span[text()="Gönder"]]'))
+    wait.until(
+        EC.presence_of_all_elements_located((By.XPATH, '//a[contains(@href, "/opportunities/")]'))
     )
 
-    print("Clicked Submit button.")
+    print("Fırsatlarım sayfası açıldı.")
+
+    input("Lütfen istediğiniz filtreleri seçin ve ENTER basın...")
+
+    print("Tüm ilanlar alınıyor...")
+
+    anchors = driver.find_elements(By.XPATH, '//a[contains(@href, "/opportunities/")]')
+    links = [a.get_attribute('href') for a in anchors]
     
-    submit_button.click()
+    if (len(links)) == 0:
+        print("Bu filtrede iş ilanı yok.")
+        continue
+
+    for (inx, link) in enumerate(links):
+        print(f"{link[::-1][:8][::-1]} ilanı açılıyor...")
+        
+        driver.get(f"{link}")
+        
+        reject_button = wait.until(
+            EC.element_to_be_clickable((By.XPATH, '//button[.//span[text()="Reddet"]]'))
+        )
+        
+        print("İlan sayfası açıldı!")
+
+        reject_button.click()
+        
+        print("Reddet butonuna tıklandı.")
+        
+        radio_label = wait.until(EC.presence_of_element_located(
+            (By.XPATH, '//input[@value="13"]/ancestor::label')
+        ))
+        driver.execute_script("arguments[0].scrollIntoView({block: 'center'});", radio_label)
+        driver.execute_script("arguments[0].click();", radio_label)
+        
+        print("\"Hizmet (meslek) uygun değil\" seçeneği seçildi.")
     
-    time.sleep(1)
-    
-    print("Opening new page...")
-    
-    time.sleep(1)
+        submit_btn = wait.until(EC.element_to_be_clickable(
+            (By.XPATH, '//button[.//span[text()="Gönder"]]')
+        ))
+        driver.execute_script("arguments[0].scrollIntoView({block: 'center'});", submit_btn)
+        driver.execute_script("arguments[0].click();", submit_btn)
+        
+        print("Gönder butonuna tıklandı")
+        
+        if (inx >= len(links) - 1):
+            print("Alınabilen tüm ilanlar dolaşıldı. Ana ekrana yönlendiriliyor.")
+        else:
+            print(f"Sıradaki ilan açılıyor...")
+            time.sleep(1)
